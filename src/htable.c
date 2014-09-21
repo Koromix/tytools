bc9eafef3084c70fd16f3e39d61d5338a29acf39
Niels Martignène <niels.martignene@gmail.com>
2014-09-21 11:23:18 +0200

    Add basic fixed-size hash table implementation

---
+++ src/htable.c
@@ -0,0 +1,91 @@
+/**
+ * ty, command-line program to manage Teensy devices
+ * http://github.com/Koromix/ty
+ * Copyright (C) 2014 Niels Martignène
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "common.h"
+#include "htable.h"
+
+int ty_htable_init(ty_htable *table, size_t size)
+{
+    assert(table);
+    assert(size);
+
+    table->heads = malloc(size * sizeof(*table->heads));
+    if (!table->heads)
+        return ty_error(TY_ERROR_MEMORY, NULL);
+    table->size = size;
+
+    for (size_t i = 0; i < table->size; i++)
+        table->heads[i].next = &table->heads[i];
+
+    return 0;
+}
+
+void ty_htable_release(ty_htable *table)
+{
+    assert(table);
+
+    free(table->heads);
+}
+
+ty_htable_head *ty_htable_get_head(ty_htable *table, uint32_t key)
+{
+    assert(table);
+
+    return &table->heads[key % table->size];
+}
+
+void ty_htable_add(ty_htable *table, uint32_t key, ty_htable_head *n)
+{
+    assert(table);
+    assert(n);
+
+    ty_htable_head *head = ty_htable_get_head(table, key);
+
+    n->next = head->next;
+    n->key = key;
+
+    head->next = n;
+}
+
+void ty_htable_remove(ty_htable_head *head)
+{
+    assert(head);
+
+    if (head == head->next || !head->next)
+        return;
+
+    ty_htable_head *prev = head->next;
+    while (prev->next != head)
+        prev = prev->next;
+
+    prev->next = head->next;
+
+    head->next = NULL;
+}
+
+uint32_t ty_htable_hash_str(const char *s)
+{
+    assert(s);
+
+    uint32_t hash = 0;
+    while (*s)
+        hash = hash * 101 + (unsigned char)*s++;
+
+    return hash;
+}
---
+++ src/htable.h
@@ -0,0 +1,62 @@
+/**
+ * ty, command-line program to manage Teensy devices
+ * http://github.com/Koromix/ty
+ * Copyright (C) 2014 Niels Martignène
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TY_HTABLE_H
+#define TY_HTABLE_H
+
+#include "common.h"
+
+TY_C_BEGIN
+
+typedef struct ty_htable_head {
+    struct ty_htable_head *next;
+    uint32_t key;
+} ty_htable_head;
+
+typedef struct ty_htable {
+    size_t size;
+    ty_htable_head *heads;
+} ty_htable;
+
+int ty_htable_init(ty_htable *table, size_t size);
+void ty_htable_release(ty_htable *table);
+
+ty_htable_head *ty_htable_get_head(ty_htable *table, uint32_t key);
+
+void ty_htable_add(ty_htable *table, uint32_t key, ty_htable_head *head);
+void ty_htable_remove(ty_htable_head *head);
+
+uint32_t ty_htable_hash_str(const char* s);
+
+#define ty_htable_entry(head, type, member) \
+    ((type *)((char *)(head) - (size_t)(&((type *)0)->member)))
+
+#define ty_htable_foreach(cur, table) \
+    for (ty_htable_head *TY_UNIQUE_ID(head) = (table)->heads; TY_UNIQUE_ID(head) < (table)->heads + (table)->size; TY_UNIQUE_ID(head)++) \
+        for (ty_htable_head *cur = TY_UNIQUE_ID(head)->next, *TY_UNIQUE_ID(next) = cur->next; cur != TY_UNIQUE_ID(head); \
+             cur = TY_UNIQUE_ID(next), TY_UNIQUE_ID(next) = cur->next)
+
+#define ty_htable_foreach_hash(cur, table, k) \
+    for (ty_htable_head *TY_UNIQUE_ID(head) = ty_htable_get_head((table), (k)), *cur = TY_UNIQUE_ID(head)->next, *TY_UNIQUE_ID(next) = cur->next; \
+         cur != TY_UNIQUE_ID(head); cur = TY_UNIQUE_ID(next), TY_UNIQUE_ID(next) = cur->next) \
+        if (cur->key == (k))
+
+TY_C_END
+
+#endif
