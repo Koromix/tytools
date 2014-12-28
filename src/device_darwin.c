40ee83a4096fa2325605fb1aff3c2645d0773989
Niels Martignène <niels.martignene@gmail.com>
2014-12-28 12:22:42 +0100

    Add empty/stub device_darwin.c

---
+++ src/device_darwin.c
@@ -0,0 +1,23 @@
+/**
+ * ty, command-line program to manage Teensy devices
+ * http://github.com/Koromix/ty
+ * Copyright (C) 2014 Niels Martignène
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "ty/common.h"
+#include "compat.h"
+#include "ty/device.h"
+
