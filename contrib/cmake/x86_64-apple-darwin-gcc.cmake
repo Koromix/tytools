6c84a0921668dcc9ec12c2956c6ded120ec11651
Niels Martignène <niels.martignene@gmail.com>
2015-01-12 21:16:50 +0100

    Add osxcross toolchain file
    Cross-compiling for OSX on a Linux host is great, thanks to jbliesener
    for pointing it out!

---
+++ contrib/cmake/x86_64-apple-darwin-gcc.cmake
@@ -0,0 +1,41 @@
+# See here: http://www.cmake.org/Wiki/CmakeMingw
+
+set(CMAKE_SYSTEM_NAME Darwin)
+
+find_file(CMAKE_C_COMPILER
+    NAMES x86_64-apple-darwin14-gcc
+          x86_64-apple-darwin13-gcc
+          x86_64-apple-darwin12-gcc
+          x86_64-apple-darwin11-gcc)
+
+if(CMAKE_C_COMPILER)
+    string(REGEX REPLACE "-gcc$" "" DARWIN_TOOLCHAIN "${CMAKE_C_COMPILER}")
+
+    set(CMAKE_CXX_COMPILER "${DARWIN_TOOLCHAIN}-g++")
+    set(PKG_CONFIG_EXECUTABLE "${DARWIN_TOOLCHAIN}-pkg-config")
+
+    # Kind of a cheat, this path cannot exist (unless CMAKE_C_COMPILER is a directory)
+    # but ABSOLUTE does not care about it (REALPATH would)
+    get_filename_component(DARWIN_SDK "${CMAKE_C_COMPILER}/../../SDK" ABSOLUTE)
+
+    find_file(CMAKE_FIND_ROOT_PATH
+        NAMES MacOSX10.10.sdk
+              MacOSX10.9.sdk
+              MacOSX10.8.sdk
+              MacOSX10.7.sdk
+        PATHS "${DARWIN_SDK}"
+        NO_DEFAULT_PATH)
+
+    if(NOT CMAKE_FIND_ROOT_PATH)
+        message(FATAL_ERROR "Cannot find Apple SDK")
+    endif()
+else()
+    message(FATAL_ERROR "Cannot find Apple GCC compiler")
+endif()
+
+# Adjust the default behaviour of the FIND_XXX() commands:
+#  - search headers and libraries in the target environment
+#  - search programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
