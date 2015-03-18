ae9ec05651de060224c624bb30e94208c3f751f8
Niels Martignène <niels.martignene@gmail.com>
2015-03-18 16:13:45 +0100

    Restore CMake building on Windows

--- scripts/git-version.bat
+++ scripts/git-version.bat
@@ -3,7 +3,10 @@
 setlocal
 
 set OUTFILE=%1
-if [%OUTFILE%] == [] set OUTFILE=CON
+if NOT [%OUTFILE%] == [] (
+    call "%~f0" >"%OUTFILE%"
+    goto end
+)
 
 (
     for /F %%t in ('git describe --tags') do (
@@ -14,24 +17,22 @@ if [%OUTFILE%] == [] set OUTFILE=CON
 goto src_version
 
 :git_version
-(
-    echo /*
-    echo * This Source Code Form is subject to the terms of the Mozilla Public
-    echo * License, v. 2.0. If a copy of the MPL was not distributed with this
-    echo * file, You can obtain one at http://mozilla.org/MPL/2.0/.
-    echo */
-    echo.
-    echo #ifndef TY_VERSION_H
-    echo #define TY_VERSION_H
-    echo.
-    echo #define TY_VERSION "%tag:~1%"
-    echo.
-    echo #endif
-) >%OUTFILE%
+echo /*
+echo * This Source Code Form is subject to the terms of the Mozilla Public
+echo * License, v. 2.0. If a copy of the MPL was not distributed with this
+echo * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+echo */
+echo.
+echo #ifndef TY_VERSION_H
+echo #define TY_VERSION_H
+echo.
+echo #define TY_VERSION "%tag:~1%"
+echo.
+echo #endif
 goto end
 
 :src_version
-type "%~dp0\..\src\version.h" >%OUTFILE%
+type "%~dp0\..\src\version.h"
 goto end
 
 :end
