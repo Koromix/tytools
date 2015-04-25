876085036dac1bf5a54598a180a844f30a46df0c
Niels Martignène <niels.martignene@gmail.com>
2015-04-25 18:22:17 +0200

    Remove unused system functions

--- src/system.c
+++ src/system.c
@@ -11,10 +11,6 @@
 #include <sys/types.h>
 #include "ty/system.h"
 
-#ifdef HAVE_FSTATAT
-int _ty_statat(int fd, const char *path, ty_file_info *info, bool follow);
-#endif
-
 int ty_adjust_timeout(int timeout, uint64_t start)
 {
     if (timeout < 0)
@@ -25,78 +21,6 @@ int ty_adjust_timeout(int timeout, uint64_t start)
     if (now > start + (uint64_t)timeout)
         return 0;
     return (int)(start + (uint64_t)timeout - now);
-}
-
-bool ty_path_is_absolute(const char *path)
-{
-    assert(path);
-
-#ifdef _WIN32
-    if (((path[0] >= 'a' && path[0] <= 'z') || (path[0] >= 'A' && path[0] <= 'Z')) && path[1] == ':')
-        path += 2;
-#endif
-
-    return strchr(TY_PATH_SEPARATORS, path[0]);
-}
-
-int ty_path_split(const char *path, char **rdirectory, char **rname)
-{
-    assert(path && path[0]);
-    assert(rdirectory || rname);
-
-    const char *path_;
-    const char *end, *base;
-    char *directory = NULL, *name = NULL;
-    int r;
-
-    path_ = path;
-#ifdef _WIN32
-    if (((path[0] >= 'a' && path[0] <= 'z') || (path[0] >= 'A' && path[0] <= 'Z')) && path[1] == ':')
-        path_ += 2;
-#endif
-
-    end = path_ + strlen(path_);
-    while (end > path_ + 1 && strchr(TY_PATH_SEPARATORS, end[-1]))
-        end--;
-
-    base = end;
-    while (base > path_ && !strchr(TY_PATH_SEPARATORS, base[-1]))
-        base--;
-
-    if (rname) {
-        name = strndup(base, (size_t)(end - base));
-        if (!name) {
-            r = ty_error(TY_ERROR_MEMORY, NULL);
-            goto error;
-        }
-    }
-
-    if (rdirectory) {
-        if (base > path) {
-            while (base > path_ + 1 && strchr(TY_PATH_SEPARATORS, base[-1]))
-                base--;
-
-            directory = strndup(path, (size_t)(base - path));
-        } else {
-            directory = strdup(".");
-        }
-        if (!directory) {
-            r = ty_error(TY_ERROR_MEMORY, NULL);
-            goto error;
-        }
-
-        *rdirectory = directory;
-    }
-
-    if (rname)
-        *rname = name;
-
-    return 0;
-
-error:
-    free(name);
-    free(directory);
-    return r;
 }
 
 void ty_descriptor_set_clear(ty_descriptor_set *set)
