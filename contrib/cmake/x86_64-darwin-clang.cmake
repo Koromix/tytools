8c54b99ad733abe34b5eed6095855db8d246152a
Niels Martignène <niels.martignene@gmail.com>
2015-10-31 11:56:50 +0100

    Improve the Darwin cross-compilation toolchain file
    osxcross has provided xcrun for a few months now, so we can just
    dump the manual and fragile path extraction process.

--- contrib/cmake/x86_64-darwin-clang.cmake
+++ contrib/cmake/x86_64-darwin-clang.cmake
@@ -2,42 +2,26 @@
 
 set(CMAKE_SYSTEM_NAME Darwin)
 
-find_file(CMAKE_C_COMPILER
-    NAMES x86_64-apple-darwin14-clang
-          x86_64-apple-darwin13-clang
-          x86_64-apple-darwin12-clang
-          x86_64-apple-darwin11-clang)
+# Go to https://github.com/tpoechtrager/osxcross and fall in love
+execute_process(COMMAND xcrun --show-sdk-path
+    OUTPUT_VARIABLE CMAKE_FIND_ROOT_PATH OUTPUT_STRIP_TRAILING_WHITESPACE)
+execute_process(COMMAND xcrun -f clang
+    OUTPUT_VARIABLE CMAKE_C_COMPILER OUTPUT_STRIP_TRAILING_WHITESPACE)
+execute_process(COMMAND xcrun -f clang++
+    OUTPUT_VARIABLE CMAKE_CXX_COMPILER OUTPUT_STRIP_TRAILING_WHITESPACE)
+execute_process(COMMAND xcrun -f pkg-config
+    OUTPUT_VARIABLE PKG_CONFIG_EXECUTABLE OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-if(CMAKE_C_COMPILER)
-    string(REGEX REPLACE "-clang$" "" DARWIN_TOOLCHAIN "${CMAKE_C_COMPILER}")
-
-    set(CMAKE_CXX_COMPILER "${DARWIN_TOOLCHAIN}-clang++")
-    set(PKG_CONFIG_EXECUTABLE "${DARWIN_TOOLCHAIN}-pkg-config")
-
-    string(REGEX REPLACE "^.*-darwin([0-9]+)-.*$" "\\1" DARWIN_MAJOR_VERSION "${CMAKE_C_COMPILER}")
-
-    # CMAKE_SYSTEM_VERSION is needed to enable the OSX RPATH support, without it
-    # Platform/Darwin.cmake does not set CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG
-    set(CMAKE_SYSTEM_VERSION "${DARWIN_MAJOR_VERSION}.0")
-
-    # Kind of a cheat, this path cannot exist (unless CMAKE_C_COMPILER is a directory)
-    # but ABSOLUTE does not care about it (REALPATH would)
-    get_filename_component(DARWIN_SDK "${CMAKE_C_COMPILER}/../../SDK" ABSOLUTE)
-
-    find_file(CMAKE_FIND_ROOT_PATH
-        NAMES MacOSX10.10.sdk
-              MacOSX10.9.sdk
-              MacOSX10.8.sdk
-              MacOSX10.7.sdk
-        PATHS "${DARWIN_SDK}"
-        NO_DEFAULT_PATH)
-
-    if(NOT CMAKE_FIND_ROOT_PATH)
-        message(FATAL_ERROR "Cannot find Apple SDK")
-    endif()
-else()
+if(NOT CMAKE_FIND_ROOT_PATH)
+    message(FATAL_ERROR "Cannot find Apple SDK")
+endif()
+if(NOT CMAKE_C_COMPILER)
     message(FATAL_ERROR "Cannot find Apple Clang compiler")
 endif()
+
+# Know a better way to get the Darwin version?
+string(REGEX REPLACE "^.*-darwin([0-9]+)-.*$" "\\1" DARWIN_MAJOR_VERSION "${CMAKE_C_COMPILER}")
+set(CMAKE_SYSTEM_VERSION "${DARWIN_MAJOR_VERSION}.0")
 
 # Adjust the default behaviour of the FIND_XXX() commands:
 #  - search headers and libraries in the target environment
