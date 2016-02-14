ea3fee315f385cbdb4885cc3fab47eff9b100ebc
Niels Martignène <niels.martignene@gmail.com>
2016-02-14 15:32:39 +0100

    Remove 'useless' checks from SettingsDatabase

--- tyqt/database.cc
+++ tyqt/database.cc
@@ -13,32 +13,20 @@ using namespace std;
 
 void SettingsDatabase::put(const QString &key, const QVariant &value)
 {
-    if (!settings_)
-        return;
-
     settings_->setValue(key, value);
 }
 
 void SettingsDatabase::remove(const QString &key)
 {
-    if (!settings_)
-        return;
-
     settings_->remove(key);
 }
 
 QVariant SettingsDatabase::get(const QString &key, const QVariant &default_value) const
 {
-    if (!settings_)
-        return default_value;
-
     return settings_->value(key, default_value);
 }
 
 void SettingsDatabase::clear()
 {
-    if (!settings_)
-        return;
-
     settings_->clear();
 }
