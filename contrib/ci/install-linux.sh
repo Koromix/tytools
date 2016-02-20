ac4048fb659305d3d1c10a89502ee124e66d5bb7
Niels Martignène <niels.martignene@gmail.com>
2016-02-20 22:52:48 +0100

    Add Travis / Bintray build integration
    The main catalyst for this is that my MacBook does not work at
    the moment so I can't build anything for OSX.
    
    The OSX builds are published on Bintray, I hope to do the same for
    Windows using AppVeyor later.

---
+++ contrib/ci/install-linux.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo apt-get install -qqy cmake libudev-dev qtbase5-dev
---
+++ contrib/ci/install-osx.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget https://bintray.com/artifact/download/koromix/ty/qt-$QT_VERSION-x86_64-darwin-clang-static.txz
+tar xJf qt-$QT_VERSION-x86_64-darwin-clang-static.txz
