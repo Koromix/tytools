c934b017702971550d10d8c243a2c357c22497ff
Niels Martignène <niels.martignene@gmail.com>
2016-05-27 05:18:01 +0200

    Minor changes to .travis.yml

--- contrib/ci/install-win32.sh
+++ contrib/ci/install-win32.sh
@@ -2,5 +2,5 @@
 
 sudo apt-get install -qqy cmake gcc-mingw-w64-i686 g++-mingw-w64-i686
 
-wget https://bintray.com/artifact/download/koromix/ty/qtbase-$QT_VERSION-i686-w64-mingw32-static.txz
-tar xJf qtbase-$QT_VERSION-i686-w64-mingw32-static.txz
+wget https://bintray.com/artifact/download/koromix/ty/$QT_PACKAGE
+tar xJf $QT_PACKAGE
