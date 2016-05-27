c5b082ba38489178208edc01f1e31cfb56d3913b
Niels Martignène <niels.martignene@gmail.com>
2016-05-27 05:18:01 +0200

    Minor changes to .travis.yml

--- contrib/ci/install-osx.sh
+++ contrib/ci/install-osx.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-wget https://bintray.com/artifact/download/koromix/ty/qtbase-$QT_VERSION-x86_64-darwin-clang-static.txz
-tar xJf qtbase-$QT_VERSION-x86_64-darwin-clang-static.txz
+wget https://bintray.com/artifact/download/koromix/ty/$QT_PACKAGE
+tar xJf $QT_PACKAGE
--- contrib/ci/install-win64.sh
+++ contrib/ci/install-win64.sh
@@ -2,5 +2,5 @@
 
 sudo apt-get install -qqy cmake gcc-mingw-w64-x86-64 g++-mingw-w64-x86-64
 
-wget https://bintray.com/artifact/download/koromix/ty/qtbase-$QT_VERSION-x86_64-w64-mingw32-static.txz
-tar xJf qtbase-$QT_VERSION-x86_64-w64-mingw32-static.txz
+wget https://bintray.com/artifact/download/koromix/ty/$QT_PACKAGE
+tar xJf $QT_PACKAGE
