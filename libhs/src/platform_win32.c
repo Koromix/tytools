2a192593d83abece945775555e10ba52a4774e1d
Niels Martignène <niels.martignene@gmail.com>
2016-07-09 19:09:43 +0200

    Resolve RtlGetVersion() on first use

--- libhs/src/platform_win32.c
+++ libhs/src/platform_win32.c
@@ -27,28 +27,17 @@
 #include <windows.h>
 #include "hs/platform.h"
 
-typedef LONG NTAPI RtlGetVersion_func(OSVERSIONINFOW *info);
 typedef ULONGLONG WINAPI GetTickCount64_func(void);
+typedef LONG NTAPI RtlGetVersion_func(OSVERSIONINFOW *info);
 
-static ULONGLONG WINAPI GetTickCount64_fallback(void);
-
-static GetTickCount64_func *GetTickCount64_;
-static RtlGetVersion_func *RtlGetVersion_;
-
-_HS_INIT()
-{
-    GetTickCount64_ = (GetTickCount64_func *)GetProcAddress(GetModuleHandle("kernel32.dll"), "GetTickCount64");
-    if (!GetTickCount64_)
-        GetTickCount64_ = GetTickCount64_fallback;
-
-    RtlGetVersion_ = (RtlGetVersion_func *)GetProcAddress(GetModuleHandle("ntdll.dll"), "RtlGetVersion");
-    assert(RtlGetVersion_);
-}
+static ULONGLONG WINAPI GetTickCount64_resolve(void);
+static GetTickCount64_func *GetTickCount64_ = GetTickCount64_resolve;
+static LONG NTAPI RtlGetVersion_resolve(OSVERSIONINFOW *info);
+static RtlGetVersion_func *RtlGetVersion_ = RtlGetVersion_resolve;
 
 static ULONGLONG WINAPI GetTickCount64_fallback(void)
 {
     static LARGE_INTEGER freq;
-
     LARGE_INTEGER now;
     BOOL ret _HS_POSSIBLY_UNUSED;
 
@@ -61,6 +50,15 @@ static ULONGLONG WINAPI GetTickCount64_fallback(void)
     assert(ret);
 
     return (ULONGLONG)now.QuadPart * 1000 / (ULONGLONG)freq.QuadPart;
+}
+
+static ULONGLONG WINAPI GetTickCount64_resolve(void)
+{
+    GetTickCount64_ = (GetTickCount64_func *)GetProcAddress(GetModuleHandle("kernel32.dll"), "GetTickCount64");
+    if (!GetTickCount64_)
+        GetTickCount64_ = GetTickCount64_fallback;
+
+    return GetTickCount64_();
 }
 
 uint64_t hs_millis(void)
@@ -116,6 +114,13 @@ const char *hs_win32_strerror(DWORD err)
     }
 
     return buf;
+}
+
+static LONG NTAPI RtlGetVersion_resolve(OSVERSIONINFOW *info)
+{
+    RtlGetVersion_ = (RtlGetVersion_func *)GetProcAddress(GetModuleHandle("ntdll.dll"),
+                                                          "RtlGetVersion");
+    return RtlGetVersion_(info);
 }
 
 uint32_t hs_win32_version(void)
