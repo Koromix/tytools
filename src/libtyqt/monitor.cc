Koromix/tytools

commit 08c69df75f57901f577b6c41f7506820702e7cd8

Load board settings when UNIQUE capability is acquired

src/libtyqt/monitor.cc
@@ -343,13 +343,8 @@ void Monitor::handleAddedEvent(ty_board *board)
     };
     auto ptr = make_shared<BoardSharedEnabler>(board);
 
-    if (ptr->hasCapability(TY_BOARD_CAPABILITY_UNIQUE)) {
-        ptr->setDatabase(db_.subDatabase(ptr->id()));
-        ptr->setCache(cache_.subDatabase(ptr->id()));
-        ptr->serial_log_file_.setFileName(findLogFilename(ptr->id(), 3));
-    } else {
-        ptr->serial_log_file_.setFileName(findLogFilename("UNKNOWN", 6));
-    }
+    if (ptr->hasCapability(TY_BOARD_CAPABILITY_UNIQUE))
+        configureBoardDatabase(*ptr);
     ptr->loadSettings(this);
 
     ptr->setThreadPool(pool_);
@@ -359,6 +354,11 @@ void Monitor::handleAddedEvent(ty_board *board)
         refreshBoardItem(findBoardIterator(board));
     });
     connect(ptr.get(), &Board::interfacesChanged, this, [=]() {
+        if (db_.isValid() && !ptr->database().isValid() &&
+                ptr->hasCapability(TY_BOARD_CAPABILITY_UNIQUE)) {
+            configureBoardDatabase(*ptr);
+            ptr->loadSettings(this);
+        }
         refreshBoardItem(findBoardIterator(board));
     });
     connect(ptr.get(), &Board::statusChanged, this, [=]() {
@@ -399,6 +399,13 @@ void Monitor::removeBoardItem(iterator it)
     beginRemoveRows(QModelIndex(), it - boards_.begin(), it - boards_.begin());
     boards_.erase(it);
     endRemoveRows();
+}
+
+void Monitor::configureBoardDatabase(Board &board)
+{
+    board.setDatabase(db_.subDatabase(board.id()));
+    board.setCache(cache_.subDatabase(board.id()));
+    board.serial_log_file_.setFileName(findLogFilename(board.id(), 3));
 }
 
 QString Monitor::findLogFilename(const QString &id, unsigned int max)

include/tyqt/database.hpp
@@ -52,6 +52,8 @@ public:
     void setDatabase(Database *db) { db_ = db; }
     Database *database() const { return db_; }
 
+    bool isValid() const { return db_; }
+
     void setGroup(const QString &group);
     QString group() const { return group_; }
 

include/tyqt/monitor.hpp
@@ -124,6 +124,7 @@ private:
     void refreshBoardItem(iterator it);
     void removeBoardItem(iterator it);
 
+    void configureBoardDatabase(Board &board);
     QString findLogFilename(const QString &tpl, unsigned int max);
 };
 

commit ca4a70a326373bdf0ff74de6b2e59195bcd15e50

Fix Board leak introduced a few commits ago
This was introduced in 08c69df75f57901f577b6c41f7506820702e7cd8,
be careful when capturing shared pointers in a lambda

src/libtyqt/monitor.cc
@@ -341,41 +341,43 @@ void Monitor::handleAddedEvent(ty_board *board)
         BoardSharedEnabler(ty_board *board)
             : Board(board) {}
     };
-    auto ptr = make_shared<BoardSharedEnabler>(board);
+    auto board_wrapper_ptr = make_shared<BoardSharedEnabler>(board);
+    auto board_wrapper = board_wrapper_ptr.get();
 
-    if (ptr->hasCapability(TY_BOARD_CAPABILITY_UNIQUE))
-        configureBoardDatabase(*ptr);
-    ptr->loadSettings(this);
+    if (board_wrapper->hasCapability(TY_BOARD_CAPABILITY_UNIQUE))
+        configureBoardDatabase(*board_wrapper);
+    board_wrapper->loadSettings(this);
 
-    ptr->setThreadPool(pool_);
-    ptr->serial_notifier_.moveToThread(&serial_thread_);
+    board_wrapper->setThreadPool(pool_);
+    board_wrapper->serial_notifier_.moveToThread(&serial_thread_);
 
-    connect(ptr.get(), &Board::infoChanged, this, [=]() {
+    connect(board_wrapper, &Board::infoChanged, this, [=]() {
         refreshBoardItem(findBoardIterator(board));
     });
-    connect(ptr.get(), &Board::interfacesChanged, this, [=]() {
-        if (db_.isValid() && !ptr->database().isValid() &&
-                ptr->hasCapability(TY_BOARD_CAPABILITY_UNIQUE)) {
-            configureBoardDatabase(*ptr);
-            ptr->loadSettings(this);
+    // Don't capture board_wrapper_ptr, this should be obvious but I made the mistake once
+    connect(board_wrapper, &Board::interfacesChanged, this, [=]() {
+        if (db_.isValid() && !board_wrapper->database().isValid() &&
+                board_wrapper->hasCapability(TY_BOARD_CAPABILITY_UNIQUE)) {
+            configureBoardDatabase(*board_wrapper);
+            board_wrapper->loadSettings(this);
         }
         refreshBoardItem(findBoardIterator(board));
     });
-    connect(ptr.get(), &Board::statusChanged, this, [=]() {
+    connect(board_wrapper, &Board::statusChanged, this, [=]() {
         refreshBoardItem(findBoardIterator(board));
     });
-    connect(ptr.get(), &Board::progressChanged, this, [=]() {
+    connect(board_wrapper, &Board::progressChanged, this, [=]() {
         refreshBoardItem(findBoardIterator(board));
     });
-    connect(ptr.get(), &Board::dropped, this, [=]() {
+    connect(board_wrapper, &Board::dropped, this, [=]() {
         removeBoardItem(findBoardIterator(board));
     });
 
     beginInsertRows(QModelIndex(), boards_.size(), boards_.size());
-    boards_.push_back(ptr);
+    boards_.push_back(board_wrapper_ptr);
     endInsertRows();
 
-    emit boardAdded(ptr.get());
+    emit boardAdded(board_wrapper);
 }
 
 void Monitor::handleChangedEvent(ty_board *board)