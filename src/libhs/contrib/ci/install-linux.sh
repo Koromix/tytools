3786228d9fda5432932faba7c0c47d201dde1748
Niels Martignène <niels.martignene@gmail.com>
2016-12-31 19:36:45 +0100

    Remove leftover libhs project files

--- src/libhs/contrib/ci/install-linux.sh
+++
@@ -1,4 +0,0 @@
-wget --no-check-certificate https://cmake.org/files/v3.4/cmake-3.4.3-Linux-x86_64.tar.gz
-tar zxf cmake-3.4.3-Linux-x86_64.tar.gz
-
-export PATH=$PWD/cmake-3.4.3-Linux-x86_64/bin:$PATH
