Koromix/tytools

commit 579c3bd8ac19860f10479d95f10187724cc23638

Fix windows.h use in libty public header

src/libty/thread.h
@@ -9,11 +9,7 @@
 #define TY_THREAD_H
 
 #include "common.h"
-#ifdef _WIN32
-    // FIXME: avoid windows.h in public headers
-    #define WIN32_LEAN_AND_MEAN
-    #include <windows.h>
-#else
+#ifndef _WIN32
     #include <pthread.h>
 #endif
 
@@ -21,7 +17,7 @@ TY_C_BEGIN
 
 typedef struct ty_thread {
 #ifdef _WIN32
-    HANDLE h;
+    void *h; // HANDLE
 #else
     pthread_t thread;
     bool init;
@@ -34,8 +30,10 @@ typedef enum ty_mutex_type {
 } ty_mutex_type;
 
 typedef struct ty_mutex {
-#ifdef _WIN32
-    CRITICAL_SECTION mutex;
+#if defined(_MSC_VER)
+    __declspec(align(4)) struct { char dummy[24]; } mutex; // CRITICAL_SECTION
+#elif defined(_WIN32)
+    struct { char dummy[24]; } __attribute__((__aligned__(4))) mutex; // CRITICAL_SECTION
 #else
     pthread_mutex_t mutex;
 #endif
@@ -43,13 +41,24 @@ typedef struct ty_mutex {
 } ty_mutex;
 
 typedef struct ty_cond {
-#ifdef _WIN32
+#if defined(_MSC_VER)
     union {
-        CONDITION_VARIABLE cv;
+        __declspec(align(4)) struct { char dummy[4]; } cv; // CONDITION_VARIABLE
         struct {
-            HANDLE ev;
+            void *ev; // HANDLE
 
-            CRITICAL_SECTION mutex;
+            __declspec(align(4)) struct { char dummy[24]; } mutex; // CRITICAL_SECTION
+            unsigned int waiting;
+            unsigned int wakeup;
+        } xp;
+    };
+#elif defined(_WIN32)
+    union {
+        struct { char dummy[4]; } __attribute__((__aligned__(4))) cv; // CONDITION_VARIABLE
+        struct {
+            void *ev; // HANDLE
+
+            struct { char dummy[24]; } __attribute__((__aligned__(4))) mutex; // CRITICAL_SECTION
             unsigned int waiting;
             unsigned int wakeup;
         } xp;

src/libty/thread_win32.c
@@ -6,6 +6,8 @@
  */
 
 #include "common_priv.h"
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
 #include <process.h>
 #include "system.h"
 #include "thread.h"
@@ -100,7 +102,7 @@ int ty_mutex_init(ty_mutex *mutex, ty_mutex_type type)
 {
     TY_UNUSED(type);
 
-    InitializeCriticalSection(&mutex->mutex);
+    InitializeCriticalSection((CRITICAL_SECTION *)&mutex->mutex);
     mutex->init = true;
 
     return 0;
@@ -111,43 +113,43 @@ void ty_mutex_release(ty_mutex *mutex)
     if (!mutex->init)
         return;
 
-    DeleteCriticalSection(&mutex->mutex);
+    DeleteCriticalSection((CRITICAL_SECTION *)&mutex->mutex);
     mutex->init = false;
 }
 
 void ty_mutex_lock(ty_mutex *mutex)
 {
-    EnterCriticalSection(&mutex->mutex);
+    EnterCriticalSection((CRITICAL_SECTION *)&mutex->mutex);
 }
 
 void ty_mutex_unlock(ty_mutex *mutex)
 {
-    LeaveCriticalSection(&mutex->mutex);
+    LeaveCriticalSection((CRITICAL_SECTION *)&mutex->mutex);
 }
 
 static void WINAPI WakeConditionVariable_fallback(CONDITION_VARIABLE *cv)
 {
     ty_cond *cond = (ty_cond *)cv;
 
-    EnterCriticalSection(&cond->xp.mutex);
+    EnterCriticalSection((CRITICAL_SECTION *)&cond->xp.mutex);
 
     if (cond->xp.wakeup < cond->xp.waiting)
         cond->xp.wakeup++;
     SetEvent(cond->xp.ev);
 
-    LeaveCriticalSection(&cond->xp.mutex);
+    LeaveCriticalSection((CRITICAL_SECTION *)&cond->xp.mutex);
 }
 
 static void WINAPI WakeAllConditionVariable_fallback(CONDITION_VARIABLE *cv)
 {
     ty_cond *cond = (ty_cond *)cv;
 
-    EnterCriticalSection(&cond->xp.mutex);
+    EnterCriticalSection((CRITICAL_SECTION *)&cond->xp.mutex);
 
     cond->xp.wakeup = cond->xp.waiting;
     SetEvent(cond->xp.ev);
 
-    LeaveCriticalSection(&cond->xp.mutex);
+    LeaveCriticalSection((CRITICAL_SECTION *)&cond->xp.mutex);
 }
 
 static DWORD adjust_timeout_win32(DWORD timeout, uint64_t start)
@@ -172,15 +174,15 @@ static BOOL WINAPI SleepConditionVariableCS_fallback(CONDITION_VARIABLE *cv,
     DWORD wret;
 
     while (true) {
-        EnterCriticalSection(&cond->xp.mutex);
+        EnterCriticalSection((CRITICAL_SECTION *)&cond->xp.mutex);
         if (!cond->xp.wakeup)
             break;
-        LeaveCriticalSection(&cond->xp.mutex);
+        LeaveCriticalSection((CRITICAL_SECTION *)&cond->xp.mutex);
     }
 
     cond->xp.waiting++;
 
-    LeaveCriticalSection(&cond->xp.mutex);
+    LeaveCriticalSection((CRITICAL_SECTION *)&cond->xp.mutex);
     LeaveCriticalSection(mutex);
 
     start = ty_millis();
@@ -188,7 +190,7 @@ restart:
     wret = WaitForSingleObject(cond->xp.ev, adjust_timeout_win32(timeout, start));
     assert(wret == WAIT_OBJECT_0 || wret == WAIT_TIMEOUT);
 
-    EnterCriticalSection(&cond->xp.mutex);
+    EnterCriticalSection((CRITICAL_SECTION *)&cond->xp.mutex);
 
     if (cond->xp.wakeup) {
         if (!--cond->xp.wakeup)
@@ -197,12 +199,12 @@ restart:
     } else if (wret == WAIT_TIMEOUT) {
         signaled = false;
     } else {
-        LeaveCriticalSection(&cond->xp.mutex);
+        LeaveCriticalSection((CRITICAL_SECTION *)&cond->xp.mutex);
         goto restart;
     }
     cond->xp.waiting--;
 
-    LeaveCriticalSection(&cond->xp.mutex);
+    LeaveCriticalSection((CRITICAL_SECTION *)&cond->xp.mutex);
 
     EnterCriticalSection(mutex);
     return signaled;
@@ -232,13 +234,13 @@ int ty_cond_init(ty_cond *cond)
     }
 
     if (InitializeConditionVariable_) {
-        InitializeConditionVariable_(&cond->cv);
+        InitializeConditionVariable_((CONDITION_VARIABLE *)&cond->cv);
     } else {
         memset(cond, 0, sizeof(*cond));
         cond->xp.ev = CreateEvent(NULL, TRUE, FALSE, NULL);
         if (!cond->xp.ev)
             return ty_error(TY_ERROR_SYSTEM, "CreateEvent() failed: %s", ty_win32_strerror(0));
-        InitializeCriticalSection(&cond->xp.mutex);
+        InitializeCriticalSection((CRITICAL_SECTION *)&cond->xp.mutex);
     }
     cond->init = true;
 
@@ -252,7 +254,7 @@ void ty_cond_release(ty_cond *cond)
 
     // Apparently, there is no need for a DeleteConditionVariable() on Windows >= Vista
     if (!WakeConditionVariable_) {
-        DeleteCriticalSection(&cond->xp.mutex);
+        DeleteCriticalSection((CRITICAL_SECTION *)&cond->xp.mutex);
         CloseHandle(cond->xp.ev);
     }
     cond->init = false;
@@ -260,16 +262,17 @@ void ty_cond_release(ty_cond *cond)
 
 void ty_cond_signal(ty_cond *cond)
 {
-    WakeConditionVariable_(&cond->cv);
+    WakeConditionVariable_((CONDITION_VARIABLE *)&cond->cv);
 }
 
 void ty_cond_broadcast(ty_cond *cond)
 {
-    WakeAllConditionVariable_(&cond->cv);
+    WakeAllConditionVariable_((CONDITION_VARIABLE *)&cond->cv);
 }
 
 bool ty_cond_wait(ty_cond *cond, ty_mutex *mutex, int timeout)
 {
-    return SleepConditionVariableCS_(&cond->cv, &mutex->mutex,
+    return SleepConditionVariableCS_((CONDITION_VARIABLE *)&cond->cv,
+                                     (CRITICAL_SECTION *)&mutex->mutex,
                                      timeout >= 0 ? (DWORD)timeout : INFINITE);
 }

commit 49fe8cb8b397d751caadb8139bce403e0e26fb48

Fix Win64 crash introduced in 579c3bd
Our dummy types were too small on Win64. That was a stupid mistake.

src/libty/thread.h
@@ -24,11 +24,25 @@ typedef struct ty_thread {
 #endif
 } ty_thread;
 
-typedef struct ty_mutex {
-#if defined(_MSC_VER)
-    __declspec(align(4)) struct { char dummy[24]; } mutex; // CRITICAL_SECTION
+/* Define correctly-sized dummy types to get CRITICAL_SECTION and CONDITION_VARIABLE
+   without including windows.h. */
+#if defined(_MSC_VER) && defined(_WIN64)
+typedef __declspec(align(8)) struct { char dummy[40]; } TY_WIN32_CRITICAL_SECTION;
+typedef __declspec(align(8)) struct { char dummy[8]; } TY_WIN32_CONDITION_VARIABLE;
+#elif defined(_MSC_VER)
+typedef __declspec(align(4)) struct { char dummy[24]; } TY_WIN32_CRITICAL_SECTION;
+typedef __declspec(align(4)) struct { char dummy[4]; } TY_WIN32_CONDITION_VARIABLE;
+#elif defined(_WIN64)
+typedef struct { char dummy[40]; } __attribute__((__aligned__(8))) TY_WIN32_CRITICAL_SECTION;
+typedef struct { char dummy[8]; } __attribute__((__aligned__(8))) TY_WIN32_CONDITION_VARIABLE;
 #elif defined(_WIN32)
-    struct { char dummy[24]; } __attribute__((__aligned__(4))) mutex; // CRITICAL_SECTION
+typedef struct { char dummy[24]; } __attribute__((__aligned__(4))) TY_WIN32_CRITICAL_SECTION;
+typedef struct { char dummy[4]; } __attribute__((__aligned__(4))) TY_WIN32_CONDITION_VARIABLE;
+#endif
+
+typedef struct ty_mutex {
+#ifdef _WIN32
+    TY_WIN32_CRITICAL_SECTION mutex;
 #else
     pthread_mutex_t mutex;
 #endif
@@ -36,24 +50,13 @@ typedef struct ty_mutex {
 } ty_mutex;
 
 typedef struct ty_cond {
-#if defined(_MSC_VER)
+#ifdef _WIN32
     union {
-        __declspec(align(4)) struct { char dummy[4]; } cv; // CONDITION_VARIABLE
+        TY_WIN32_CONDITION_VARIABLE cv;
         struct {
             void *ev; // HANDLE
 
-            __declspec(align(4)) struct { char dummy[24]; } mutex; // CRITICAL_SECTION
-            unsigned int waiting;
-            unsigned int wakeup;
-        } xp;
-    };
-#elif defined(_WIN32)
-    union {
-        struct { char dummy[4]; } __attribute__((__aligned__(4))) cv; // CONDITION_VARIABLE
-        struct {
-            void *ev; // HANDLE
-
-            struct { char dummy[24]; } __attribute__((__aligned__(4))) mutex; // CRITICAL_SECTION
+            TY_WIN32_CRITICAL_SECTION mutex;
             unsigned int waiting;
             unsigned int wakeup;
         } xp;