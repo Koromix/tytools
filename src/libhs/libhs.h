6024a8cca136428d187666c647cc95618ab157a9
Niels Martignène <niels.martignene@gmail.com>
2017-01-13 19:00:14 +0100

    Switch to single-header amalgamation for libhs

--- src/libhs/libhs.h
+++ src/libhs/libhs.h
@@ -25,6 +25,11 @@
 #ifndef HS_LIBHS_H
 #define HS_LIBHS_H
 
+/* This file provides both the interface and the implementation.
+   To instantiate the implementation,
+        #define HS_IMPLEMENTATION
+   in *ONE* source file, before #including this file. */
+
 #include "common.h"
 #include "htable.h"
 #include "list.h"
@@ -34,5 +39,42 @@
 #include "monitor.h"
 #include "platform.h"
 #include "serial.h"
+
+#ifdef HS_IMPLEMENTATION
+    #include "compat_priv.h"
+    #include "common_priv.h"
+    #include "device_priv.h"
+    #include "filter_priv.h"
+
+    #include "common.c"
+    #include "compat.c"
+    #include "device.c"
+    #include "filter.c"
+    #include "htable.c"
+    #include "monitor_common.c"
+    #include "platform.c"
+
+    #if defined(_WIN32)
+        #include "device_win32.c"
+        #include "hid_win32.c"
+        #include "monitor_win32.c"
+        #include "platform_win32.c"
+        #include "serial_win32.c"
+    #elif defined(__APPLE__)
+        #include "device_posix.c"
+        #include "hid_darwin.c"
+        #include "monitor_darwin.c"
+        #include "platform_darwin.c"
+        #include "serial_posix.c"
+    #elif defined(__linux__)
+        #include "device_posix.c"
+        #include "hid_linux.c"
+        #include "monitor_linux.c"
+        #include "platform_posix.c"
+        #include "serial_posix.c"
+    #else
+        #error "Platform not supported"
+    #endif
+#endif
 
 #endif
