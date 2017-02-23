77bb82f27d56851a64e1334d183b6c2a384177b7
Niels Martignène <niels.martignene@gmail.com>
2017-02-23 21:37:41 +0100

    Implement crash memory dumper in TyCommander on Win32

--- src/tycommander/main.cc
+++ src/tycommander/main.cc
@@ -11,6 +11,8 @@
 #ifdef _WIN32
     #define WIN32_LEAN_AND_MEAN
     #include <windows.h>
+    #include <dbghelp.h>
+    #include <shlobj.h>
     #include <fcntl.h>
     #include <io.h>
     #include <stdio.h>
@@ -32,6 +34,67 @@
 using namespace std;
 
 #ifdef _WIN32
+
+static void make_minidump(EXCEPTION_POINTERS *ex)
+{
+    auto MiniDumpWriteDump_ =
+        reinterpret_cast<decltype(MiniDumpWriteDump) *>(GetProcAddress(LoadLibrary("dbghelp"),
+                                                                       "MiniDumpWriteDump"));
+    auto SHGetFolderPath_ =
+        reinterpret_cast<decltype(SHGetFolderPath) *>(GetProcAddress(LoadLibrary("shell32"),
+                                                                     "SHGetFolderPathA"));
+    if (!MiniDumpWriteDump_ || !SHGetFolderPath_)
+        return;
+
+    char dmp_path[MAX_PATH + 256];
+    {
+        // Crash dump directory
+        if (SHGetFolderPath_(0, CSIDL_LOCAL_APPDATA, nullptr, SHGFP_TYPE_CURRENT, dmp_path) != S_OK)
+            return;
+        strcat(dmp_path, "\\CrashDumps");
+        CreateDirectory(dmp_path, nullptr);
+
+        // Executable name
+        char module_path[MAX_PATH];
+        DWORD module_len = GetModuleFileNameA(GetModuleHandle(nullptr), module_path,
+                                              sizeof(module_path));
+        if (!module_len)
+            return;
+        char *module_name = module_path + module_len;
+        while (module_name > module_path && module_name[-1] != '\\' && module_name[-1] != '/')
+            module_name--;
+
+        SYSTEMTIME st;
+        GetSystemTime(&st);
+
+        size_t path_len = strlen(dmp_path);
+        snprintf(dmp_path + path_len, sizeof(dmp_path) - path_len,
+                 "\\%s_%4d%02d%02d_%02d%02d%02d.dmp",
+                 module_name, st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond);
+    }
+
+    HANDLE h = CreateFile(dmp_path, GENERIC_WRITE, FILE_SHARE_READ, 0, CREATE_ALWAYS,
+                          FILE_ATTRIBUTE_NORMAL, 0);
+    if(h == INVALID_HANDLE_VALUE)
+        return;
+
+    MINIDUMP_EXCEPTION_INFORMATION ex_info;
+    ex_info.ThreadId = GetCurrentThreadId();
+    ex_info.ExceptionPointers = ex;
+    ex_info.ClientPointers = FALSE;
+
+    MiniDumpWriteDump_(GetCurrentProcess(), GetCurrentProcessId(), h,
+                       MINIDUMP_TYPE(MiniDumpWithIndirectlyReferencedMemory | MiniDumpScanMemory),
+                       ex ? &ex_info : nullptr, nullptr, nullptr);
+
+    CloseHandle(h);
+}
+
+static LONG CALLBACK unhandled_exception_handler(EXCEPTION_POINTERS *ex)
+{
+    make_minidump(ex);
+    return EXCEPTION_CONTINUE_SEARCH;
+}
 
 static bool reopen_stream(FILE *fp, const QString &path, const char *mode)
 {
@@ -67,6 +130,10 @@ static bool open_tycommanderc_bridge()
 
 int main(int argc, char *argv[])
 {
+#ifdef _WIN32
+    SetUnhandledExceptionFilter(unhandled_exception_handler);
+#endif
+
     hs_log_set_handler(ty_libhs_log_handler, NULL);
 
     qRegisterMetaType<ty_log_level>("ty_log_level");
