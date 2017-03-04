Koromix/tytools

commit 4469c0ab7e2714faa37c6df29e1a399d8e6a972f

Switch to string serial numbers in libty
Non-Teensy boards may use non-numeric serial numbers so we cannot
make this assumption anymore.

src/libty/class_generic.c
@@ -35,8 +35,9 @@ static int generic_update_board(ty_board_interface *iface, ty_board *board)
 {
     const char *manufacturer_string;
     const char *product_string;
+    const char *serial_number_string;
     ty_model model = 0;
-    uint64_t serial_number = 0;
+    char *serial_number = NULL;
     char *description = NULL;
     char *id = NULL;
     int r;
@@ -47,6 +48,9 @@ static int generic_update_board(ty_board_interface *iface, ty_board *board)
     product_string = iface->dev->product_string;
     if (!product_string)
         product_string = "Unknown";
+    serial_number_string = iface->dev->serial_number_string;
+    if (!serial_number_string)
+        serial_number_string = "?";
 
     // Check and update board model
     if (board->model && board->model != iface->model) {
@@ -56,13 +60,14 @@ static int generic_update_board(ty_board_interface *iface, ty_board *board)
     model = iface->model;
 
     // Check and update board serial number
-    if (iface->dev->serial_number_string) {
-        serial_number = strtoull(iface->dev->serial_number_string, NULL, 10);
-
-        if (board->serial_number && serial_number != board->serial_number) {
-            r = 0;
-            goto error;
-        }
+    if (board->serial_number && strcmp(board->serial_number, serial_number_string) != 0) {
+        r = 0;
+        goto error;
+    }
+    serial_number = strdup(serial_number_string);
+    if (!serial_number) {
+        r = ty_error(TY_ERROR_MEMORY, NULL);
+        goto error;
     }
 
     // Check and update board description
@@ -77,7 +82,7 @@ static int generic_update_board(ty_board_interface *iface, ty_board *board)
     }
 
     // Create new board unique identifier
-    r = asprintf(&id, "%"PRIu64"-%s", serial_number, manufacturer_string);
+    r = asprintf(&id, "%s-%s", serial_number, manufacturer_string);
     if (r < 0) {
         r = ty_error(TY_ERROR_MEMORY, NULL);
         goto error;
@@ -116,6 +121,7 @@ static int generic_update_board(ty_board_interface *iface, ty_board *board)
 error:
     free(id);
     free(description);
+    free(serial_number);
     return r;
 }
 

src/libty/class_teensy.c
@@ -153,7 +153,7 @@ static int teensy_load_interface(ty_board_interface *iface)
 static int teensy_update_board(ty_board_interface *iface, ty_board *board)
 {
     ty_model model = 0;
-    uint64_t serial_number = 0;
+    char *serial_number = NULL;
     char *description = NULL;
     char *id = NULL;
     int r;
@@ -172,29 +172,40 @@ static int teensy_update_board(ty_board_interface *iface, ty_board *board)
 
     // Check and update board serial number
     if (iface->dev->serial_number_string) {
+        uint64_t serial_value;
         if (ty_models[iface->model].code_size) {
-            serial_number = parse_bootloader_serial_number(iface->dev->serial_number_string);
+            serial_value = parse_bootloader_serial_number(iface->dev->serial_number_string);
         } else {
-            serial_number = strtoull(iface->dev->serial_number_string, NULL, 10);
+            serial_value = strtoull(iface->dev->serial_number_string, NULL, 10);
         }
 
-        /* We cannot uniquely identify AVR Teensy boards because the S/N is always 12345,
-           or custom ARM Teensy boards without a valid MAC address. Kind of dirty to
-           change iface in this function but it should not be a problem. */
-        if (serial_number && serial_number != 12345)
-            iface->capabilities |= 1 << TY_BOARD_CAPABILITY_UNIQUE;
+        if (serial_value) {
+            /* We cannot uniquely identify AVR Teensy boards because the S/N is always 12345,
+               or custom ARM Teensy boards without a valid MAC address. Kind of dirty to
+               change iface in this function but it should not be a problem. */
+            if (serial_value != 12345)
+                iface->capabilities |= 1 << TY_BOARD_CAPABILITY_UNIQUE;
 
-        if (board->serial_number && serial_number && serial_number != board->serial_number) {
-            /* Let boards using an old Teensyduino (before 1.19) firmware pass with a warning
-               because there is no way to interpret the serial number correctly, and the board
-               will show up as a different board if it is first plugged in bootloader mode.
-               The only way to fix this is to use Teensyduino >= 1.19. */
-            if (ty_models[iface->model].code_size && serial_number * 10 == board->serial_number) {
-                ty_log(TY_LOG_WARNING, "Upgrade board '%s' to use a recent Teensyduino version",
-                       board->tag);
-            } else {
-                r = 0;
+            r = asprintf(&serial_number, "%"PRIu64, serial_value);
+            if (r < 0) {
+                r = ty_error(TY_ERROR_MEMORY, NULL);
                 goto error;
+            }
+
+            if (board->serial_number && strcmp(serial_number, board->serial_number) != 0) {
+                uint64_t board_serial_value = strtoull(board->serial_number, NULL, 10);
+
+                /* Let boards using an old Teensyduino (before 1.19) firmware pass with a warning
+                   because there is no way to interpret the serial number correctly, and the board
+                   will show up as a different board if it is first plugged in bootloader mode.
+                   The only way to fix this is to use Teensyduino >= 1.19. */
+                if (ty_models[iface->model].code_size && serial_value == board_serial_value * 10) {
+                    ty_log(TY_LOG_WARNING, "Upgrade board '%s' with recent Teensyduino version",
+                           board->tag);
+                } else {
+                    r = 0;
+                    goto error;
+                }
             }
         }
     }
@@ -224,7 +235,7 @@ static int teensy_update_board(ty_board_interface *iface, ty_board *board)
 
     // Update board unique identifier
     if (!board->id || serial_number) {
-        r = asprintf(&id, "%"PRIu64"-Teensy", serial_number);
+        r = asprintf(&id, "%s-Teensy", serial_number ? serial_number : "?");
         if (r < 0) {
             r = ty_error(TY_ERROR_MEMORY, NULL);
             goto error;
@@ -250,6 +261,7 @@ static int teensy_update_board(ty_board_interface *iface, ty_board *board)
 error:
     free(id);
     free(description);
+    free(serial_number);
     return r;
 }
 

src/libty/board.c
@@ -60,6 +60,7 @@ void ty_board_unref(ty_board *board)
         if (board->tag != board->id)
             free(board->tag);
         free(board->id);
+        free(board->serial_number);
         free(board->location);
         free(board->description);
 
@@ -203,7 +204,7 @@ const char *ty_board_get_location(const ty_board *board)
     return board->location;
 }
 
-uint64_t ty_board_get_serial_number(const ty_board *board)
+const char *ty_board_get_serial_number(const ty_board *board)
 {
     assert(board);
     return board->serial_number;

src/libty/board.h
@@ -74,7 +74,7 @@ TY_PUBLIC int ty_board_set_tag(ty_board *board, const char *tag);
 TY_PUBLIC const char *ty_board_get_tag(const ty_board *board);
 
 TY_PUBLIC const char *ty_board_get_location(const ty_board *board);
-TY_PUBLIC uint64_t ty_board_get_serial_number(const ty_board *board);
+TY_PUBLIC const char *ty_board_get_serial_number(const ty_board *board);
 TY_PUBLIC const char *ty_board_get_description(const ty_board *board);
 
 TY_PUBLIC void ty_board_set_model(ty_board *board, ty_model model);

src/libty/board_priv.h
@@ -55,7 +55,7 @@ struct ty_board {
     char *tag;
     uint16_t vid;
     uint16_t pid;
-    uint64_t serial_number;
+    char *serial_number;
     char *description;
     char *location;
 

src/tycmd/list.c
@@ -169,7 +169,8 @@ static int list_callback(ty_board *board, ty_monitor_event event, void *udata)
     } else {
         print_field("action", "%s", action);
         print_field("tag", "%s", ty_board_get_tag(board));
-        print_field("serial", "%"PRIu64, ty_board_get_serial_number(board));
+        if (ty_board_get_serial_number(board))
+            print_field("serial", "%s", ty_board_get_serial_number(board));
         if (ty_board_get_description(board))
             print_field("description", "%s", ty_board_get_description(board));
         print_field("model", "%s", ty_models[model].name);

src/tycommander/board.cc
@@ -161,7 +161,7 @@ QString Board::location() const
     return ty_board_get_location(board_);
 }
 
-uint64_t Board::serialNumber() const
+QString Board::serialNumber() const
 {
     return ty_board_get_serial_number(board_);
 }

src/tycommander/board.hpp
@@ -103,7 +103,7 @@ public:
     QString tag() const;
     QString id() const;
     QString location() const;
-    uint64_t serialNumber() const;
+    QString serialNumber() const;
     QString description() const;
 
     std::vector<BoardInterfaceInfo> interfaces() const;

commit a899761d36f41334cfaaab96fe86ae414b0dc951

Fix libty memory leak introduced in 4469c0a

src/libty/class_generic.c
@@ -105,8 +105,10 @@ static int generic_update_board(ty_board_interface *iface, ty_board *board)
     // Everything is alright, we can commit changes
     if (model)
         board->model = model;
-    if (serial_number)
+    if (serial_number) {
+        free(board->serial_number);
         board->serial_number = serial_number;
+    }
     if (description) {
         free(board->description);
         board->description = description;

src/libty/class_teensy.c
@@ -253,8 +253,10 @@ static int teensy_update_board(ty_board_interface *iface, ty_board *board)
     // Everything is alright, we can commit changes
     if (model)
         board->model = model;
-    if (serial_number)
+    if (serial_number) {
+        free(board->serial_number);
         board->serial_number = serial_number;
+    }
     if (description) {
         free(board->description);
         board->description = description;