Koromix/tytools

commit 3d99668cfafe68767f615ef6783334915b5ec789

Make ty_models[] mutable and drop code_size

src/libty/board.c
@@ -208,12 +208,6 @@ void ty_board_set_model(ty_board *board, ty_model model)
     assert(board);
     assert(board->model);
 
-    if (board->model && ty_models[board->model].code_size && board->model != model) {
-        ty_log(TY_LOG_WARNING, "Cannot set model '%s' for incompatible board '%s'",
-               ty_models[model].name, board->tag);
-        return;
-    }
-
     board->model = model;
 }
 
@@ -373,11 +367,6 @@ int ty_board_upload(ty_board *board, ty_firmware *fw, ty_board_upload_progress_f
         goto cleanup;
     }
     assert(board->model);
-
-    if (fw->size > ty_models[board->model].code_size) {
-        r = ty_error(TY_ERROR_RANGE, "Firmware is too big for %s", ty_models[board->model].name);
-        goto cleanup;
-    }
 
     r = (*iface->class_vtable->upload)(iface, fw, pf, udata);
 
@@ -576,7 +565,7 @@ static int select_compatible_firmware(ty_board *board, ty_firmware **fws, unsign
     }
 
     if (fws_count > 1) {
-        return ty_error(TY_ERROR_FIRMWARE, "No firmware is compatible with '%s' (%s)",
+        return ty_error(TY_ERROR_NOT_FOUND, "No firmware is compatible with '%s' (%s)",
                         board->tag, ty_models[board->model].name);
     } else if (fw_models_count) {
         char buf[256], *ptr;
@@ -587,21 +576,34 @@ static int select_compatible_firmware(ty_board *board, ty_firmware **fws, unsign
                             i ? (i + 1 < fw_models_count ? ", " : " and ") : "",
                             ty_models[fw_models[i]].name);
 
-        return ty_error(TY_ERROR_FIRMWARE, "Firmware '%s' is only compatible with %s",
+        return ty_error(TY_ERROR_UNSUPPORTED, "Firmware '%s' is only compatible with %s",
                         fws[0]->name, buf);
     } else {
-        return ty_error(TY_ERROR_FIRMWARE, "Firmware '%s' is not compatible with '%s'",
+        return ty_error(TY_ERROR_UNSUPPORTED, "Firmware '%s' is not compatible with '%s'",
                         fws[0]->name, board->tag);
     }
 }
 
 static int upload_progress_callback(const ty_board *board, const ty_firmware *fw,
-                                    size_t uploaded, void *udata)
+                                    size_t uploaded_size, size_t flash_size, void *udata)
 {
     TY_UNUSED(board);
     TY_UNUSED(udata);
 
-    ty_progress("Uploading", uploaded, fw->size);
+    if (!uploaded_size) {
+        ty_log(TY_LOG_INFO, "Firmware: %s", fw->name);
+        if (fw->size >= 1024) {
+            ty_log(TY_LOG_INFO, "Flash usage: %zu kiB (%.1f%%)",
+                   (fw->size + 1023) / 1024,
+                   (double)fw->size / (double)flash_size * 100.0);
+        } else {
+            ty_log(TY_LOG_INFO, "Flash usage: %zu bytes (%.1f%%)",
+                   fw->size,
+                   (double)fw->size / (double)flash_size * 100.0);
+        }
+    }
+    ty_progress("Uploading", uploaded_size, fw->size);
+
     return 0;
 }
 
@@ -613,18 +615,18 @@ static void unref_upload_firmware(void *ptr)
 static int run_upload(ty_task *task)
 {
     ty_board *board = task->u.upload.board;
-    ty_firmware *fw;
+    ty_firmware *fw = NULL;
     int flags = task->u.upload.flags, r;
 
     if (flags & TY_UPLOAD_NOCHECK) {
         fw = task->u.upload.fws[0];
-    } else if (ty_model_is_real(board->model)) {
-        r = select_compatible_firmware(board, task->u.upload.fws, task->u.upload.fws_count, &fw);
-        if (r < 0)
-            return r;
     } else {
+        ty_error_mask(TY_ERROR_NOT_FOUND);
+        r = select_compatible_firmware(board, task->u.upload.fws, task->u.upload.fws_count, &fw);
+        ty_error_unmask();
         // Maybe we can identify the board and test the firmwares in bootloader mode?
-        fw = NULL;
+        if (r < 0 && r != TY_ERROR_NOT_FOUND)
+            return r;
     }
 
     ty_log(TY_LOG_INFO, "Uploading to board '%s' (%s)", board->tag, ty_models[board->model].name);
@@ -657,17 +659,6 @@ wait:
         r = select_compatible_firmware(board, task->u.upload.fws, task->u.upload.fws_count, &fw);
         if (r < 0)
             return r;
-    }
-
-    ty_log(TY_LOG_INFO, "Firmware: %s", fw->name);
-    if (fw->size >= 1024) {
-        ty_log(TY_LOG_INFO, "Flash usage: %zu kiB (%.1f%%)",
-               (fw->size + 1023) / 1024,
-               (double)fw->size / (double)ty_models[board->model].code_size * 100.0);
-    } else {
-        ty_log(TY_LOG_INFO, "Flash usage: %zu bytes (%.1f%%)",
-               fw->size,
-               (double)fw->size / (double)ty_models[board->model].code_size * 100.0);
     }
 
     r = ty_board_upload(board, fw, upload_progress_callback, NULL);

src/libty/board.h
@@ -53,7 +53,8 @@ enum {
 #define TY_UPLOAD_MAX_FIRMWARES 256
 
 typedef int ty_board_list_interfaces_func(ty_board_interface *iface, void *udata);
-typedef int ty_board_upload_progress_func(const ty_board *board, const struct ty_firmware *fw, size_t uploaded, void *udata);
+typedef int ty_board_upload_progress_func(const ty_board *board, const struct ty_firmware *fw,
+                                          size_t uploaded_size, size_t flash_size, void *udata);
 
 TY_PUBLIC const char *ty_board_capability_get_name(ty_board_capability cap);
 

src/libty/class.c
@@ -12,6 +12,23 @@
 #include "class_priv.h"
 #include "firmware.h"
 
+static ty_model_info default_models[] = {
+    {0, "Generic"},
+
+    {1, "Teensy"},
+    {1, "Teensy++ 1.0", "at90usb646"},
+    {1, "Teensy 2.0", "atmega32u4"},
+    {1, "Teensy++ 2.0", "at90usb1286"},
+    {1, "Teensy 3.0", "mk20dx128"},
+    {1, "Teensy 3.1", "mk20dx256"},
+    {1, "Teensy LC", "mkl26z64"},
+    {1, "Teensy 3.2", "mk20dx256"},
+    {1, "Teensy 3.5", "mk64fx512"},
+    {1, "Teensy 3.6", "mk66fx1m0"}
+};
+const ty_model_info *ty_models = default_models;
+const unsigned int ty_models_count = TY_COUNTOF(default_models);
+
 extern const struct _ty_class_vtable _ty_teensy_class_vtable;
 extern const struct _ty_class_vtable _ty_generic_class_vtable;
 
@@ -21,7 +38,7 @@ const struct _ty_class_vtable *_ty_class_vtables[] = {
 };
 const unsigned int _ty_class_vtables_count = TY_COUNTOF(_ty_class_vtables);
 
-ty_model ty_model_find(const char *name)
+ty_model ty_models_find(const char *name)
 {
     assert(name);
 

src/libty/class.h
@@ -20,15 +20,14 @@ typedef struct ty_model_info {
     unsigned int priority;
     const char *name;
     const char *mcu;
-    size_t code_size;
 } ty_model_info;
 
-// Keep these enums and ty_models (below) in sync
+// Keep these enums and ty_models in sync (in class.c)
 typedef enum ty_model_generic {
-    TY_MODEL_GENERIC = 1
+    TY_MODEL_GENERIC = 0
 } ty_model_generic;
 typedef enum ty_model_teensy {
-    TY_MODEL_TEENSY = 2,
+    TY_MODEL_TEENSY = 1,
     TY_MODEL_TEENSY_PP_10,
     TY_MODEL_TEENSY_20,
     TY_MODEL_TEENSY_PP_20,
@@ -40,29 +39,11 @@ typedef enum ty_model_teensy {
     TY_MODEL_TEENSY_36
 } ty_model_teensy;
 
-static const ty_model_info ty_models[] = {
-    {0, "(unknown)"},
+TY_PUBLIC extern const ty_model_info *ty_models;
+TY_PUBLIC extern const unsigned int ty_models_count;
 
-    {1, "Generic"},
-    {2, "Teensy"},
-    {2, "Teensy++ 1.0", "at90usb646", 64512},
-    {2, "Teensy 2.0", "atmega32u4", 32256},
-    {2, "Teensy++ 2.0", "at90usb1286", 130048},
-    {2, "Teensy 3.0", "mk20dx128", 131072},
-    {2, "Teensy 3.1", "mk20dx256", 262144},
-    {2, "Teensy LC", "mkl26z64", 63488},
-    {2, "Teensy 3.2", "mk20dx256", 262144},
-    {2, "Teensy 3.5", "mk64fx512", 524288},
-    {2, "Teensy 3.6", "mk66fx1m0", 1048576}
-};
-static const unsigned int ty_models_count = TY_COUNTOF(ty_models);
 
-TY_PUBLIC ty_model ty_model_find(const char *name);
-
-static inline bool ty_model_is_real(ty_model model)
-{
-    return ty_models[model].code_size;
-}
+TY_PUBLIC ty_model ty_models_find(const char *name);
 
 TY_C_END
 

src/libty/class_generic.c
@@ -32,12 +32,13 @@ static int generic_load_interface(ty_board_interface *iface)
     return 1;
 }
 
-static int generic_update_board(ty_board_interface *iface, ty_board *board)
+static int generic_update_board(ty_board_interface *iface, ty_board *board, bool new_board)
 {
+    TY_UNUSED(new_board);
+
     const char *manufacturer_string;
     const char *product_string;
     const char *serial_number_string;
-    ty_model model = 0;
     char *serial_number = NULL;
     bool unique = false;
     char *description = NULL;
@@ -55,11 +56,10 @@ static int generic_update_board(ty_board_interface *iface, ty_board *board)
         serial_number_string = "?";
 
     // Check and update board model
-    if (board->model && board->model != iface->model) {
+    if (board->model != TY_MODEL_GENERIC) {
         r = 0;
         goto error;
     }
-    model = iface->model;
 
     // Check and update board serial number
     if (board->serial_number && strcmp(board->serial_number, serial_number_string) != 0) {
@@ -109,8 +109,6 @@ static int generic_update_board(ty_board_interface *iface, ty_board *board)
     }
 
     // Everything is alright, we can commit changes
-    if (model)
-        board->model = model;
     if (serial_number) {
         free(board->serial_number);
         board->serial_number = serial_number;

src/libty/class_priv.h
@@ -19,7 +19,7 @@ TY_C_BEGIN
 
 struct _ty_class_vtable {
     int (*load_interface)(ty_board_interface *iface);
-    int (*update_board)(ty_board_interface *iface, ty_board *board);
+    int (*update_board)(ty_board_interface *iface, ty_board *board, bool new_board);
     unsigned int (*identify_models)(const struct ty_firmware *fw,
                                     ty_model *rmodels, unsigned int max_models);
 

src/libty/class_teensy.c
@@ -48,7 +48,8 @@ static ty_model identify_model(uint16_t usage)
         ty_log(TY_LOG_DEBUG, "Identified '%s' with usage value 0x%"PRIx16,
                ty_models[model].name, usage);
     } else {
-        ty_log(TY_LOG_DEBUG, "Unknown Teensy model with usage value 0x%"PRIx16, usage);
+        ty_log(TY_LOG_DEBUG, "Unknown %s model with usage value 0x%"PRIx16,
+               ty_models[TY_MODEL_TEENSY].name, usage);
     }
 
     return model;
@@ -158,7 +159,7 @@ static int teensy_load_interface(ty_board_interface *iface)
     return 1;
 }
 
-static int teensy_update_board(ty_board_interface *iface, ty_board *board)
+static int teensy_update_board(ty_board_interface *iface, ty_board *board, bool new_board)
 {
     ty_model model = 0;
     char *serial_number = NULL;
@@ -167,10 +168,10 @@ static int teensy_update_board(ty_board_interface *iface, ty_board *board)
     int r;
 
     // Check and update board model
-    if (ty_models[iface->model].code_size) {
+    if (iface->model != TY_MODEL_TEENSY) {
         model = iface->model;
 
-        if (ty_models[board->model].code_size && board->model != model) {
+        if (!new_board && board->model != TY_MODEL_TEENSY && board->model != model) {
             r = 0;
             goto error;
         }
@@ -181,7 +182,7 @@ static int teensy_update_board(ty_board_interface *iface, ty_board *board)
     // Check and update board serial number
     if (iface->dev->serial_number_string) {
         uint64_t serial_value;
-        if (ty_models[iface->model].code_size) {
+        if (iface->model != TY_MODEL_TEENSY) {
             serial_value = parse_bootloader_serial_number(iface->dev->serial_number_string);
         } else {
             serial_value = strtoull(iface->dev->serial_number_string, NULL, 10);
@@ -207,7 +208,7 @@ static int teensy_update_board(ty_board_interface *iface, ty_board *board)
                    because there is no way to interpret the serial number correctly, and the board
                    will show up as a different board if it is first plugged in bootloader mode.
                    The only way to fix this is to use Teensyduino >= 1.19. */
-                if (ty_models[iface->model].code_size && serial_value == board_serial_value * 10) {
+                if (iface->model != TY_MODEL_TEENSY && serial_value == board_serial_value * 10) {
                     ty_log(TY_LOG_WARNING, "Upgrade board '%s' with recent Teensyduino version",
                            board->tag);
                 } else {
@@ -222,13 +223,13 @@ static int teensy_update_board(ty_board_interface *iface, ty_board *board)
     {
         const char *product_string = NULL;
 
-        if (ty_models[iface->model].code_size) {
+        if (iface->model != TY_MODEL_TEENSY) {
             if (!board->description)
-                product_string = "Teensy (HalfKay)";
+                product_string = "HalfKay";
         } else if (iface->dev->product_string) {
             product_string = iface->dev->product_string;
         } else {
-            product_string = "Teensy";
+            product_string = ty_models[TY_MODEL_TEENSY].name;
         }
 
         if (product_string && (!board->description ||
@@ -243,7 +244,8 @@ static int teensy_update_board(ty_board_interface *iface, ty_board *board)
 
     // Update board unique identifier
     if (!board->id || serial_number) {
-        r = asprintf(&id, "%s-Teensy", serial_number ? serial_number : "?");
+        r = asprintf(&id, "%s-%s", serial_number ? serial_number : "?",
+                     ty_models[TY_MODEL_TEENSY].name);
         if (r < 0) {
             r = ty_error(TY_ERROR_MEMORY, NULL);
             goto error;
@@ -535,7 +537,7 @@ restart:
 }
 
 static int get_halfkay_settings(ty_model model, unsigned int *rhalfkay_version,
-                                size_t *rblock_size)
+                                size_t *rcode_size, size_t *rblock_size)
 {
     if ((model == TY_MODEL_TEENSY_PP_10 || model == TY_MODEL_TEENSY_20) &&
             !getenv("TYTOOLS_EXPERIMENTAL_BOARDS")) {
@@ -549,26 +551,43 @@ static int get_halfkay_settings(ty_model model, unsigned int *rhalfkay_version,
     switch ((ty_model_teensy)model) {
         case TY_MODEL_TEENSY_PP_10: {
             *rhalfkay_version = 1;
+            *rcode_size = 64512;
             *rblock_size = 256;
         } break;
         case TY_MODEL_TEENSY_20: {
             *rhalfkay_version = 1;
+            *rcode_size = 32256;
             *rblock_size = 128;
         } break;
         case TY_MODEL_TEENSY_PP_20: {
             *rhalfkay_version = 2;
+            *rcode_size = 130048;
             *rblock_size = 256;
         } break;
-        case TY_MODEL_TEENSY_30:
+        case TY_MODEL_TEENSY_30: {
+            *rhalfkay_version = 3;
+            *rcode_size = 131072;
+            *rblock_size = 1024;
+        } break;
         case TY_MODEL_TEENSY_31:
-        case TY_MODEL_TEENSY_32:
-        case TY_MODEL_TEENSY_35:
+        case TY_MODEL_TEENSY_32: {
+            *rhalfkay_version = 3;
+            *rcode_size = 262144;
+            *rblock_size = 1024;
+        } break;
+        case TY_MODEL_TEENSY_35: {
+            *rhalfkay_version = 3;
+            *rcode_size = 524288;
+            *rblock_size = 1024;
+        } break;
         case TY_MODEL_TEENSY_36: {
             *rhalfkay_version = 3;
+            *rcode_size = 1048576;
             *rblock_size = 1024;
         } break;
         case TY_MODEL_TEENSY_LC: {
             *rhalfkay_version = 3;
+            *rcode_size = 63488;
             *rblock_size = 512;
         } break;
 
@@ -584,16 +603,20 @@ static int get_halfkay_settings(ty_model model, unsigned int *rhalfkay_version,
 static int teensy_upload(ty_board_interface *iface, ty_firmware *fw,
                          ty_board_upload_progress_func *pf, void *udata)
 {
-    unsigned int halfkay_version = 0;
-    size_t block_size = 0;
+    unsigned int halfkay_version;
+    size_t code_size, block_size;
     int r;
 
-    r = get_halfkay_settings(iface->model, &halfkay_version, &block_size);
+    r = get_halfkay_settings(iface->model, &halfkay_version, &code_size, &block_size);
     if (r < 0)
         return r;
 
+    if (fw->size > code_size)
+        return ty_error(TY_ERROR_RANGE, "Firmware is too big for %s",
+                        ty_models[iface->model].name);
+
     if (pf) {
-        r = (*pf)(iface->board, fw, 0, udata);
+        r = (*pf)(iface->board, fw, 0, code_size, udata);
         if (r)
             return r;
     }
@@ -611,7 +634,7 @@ static int teensy_upload(ty_board_interface *iface, ty_firmware *fw,
         ty_delay(addr ? 20 : 200);
 
         if (pf) {
-            r = (*pf)(iface->board, fw, addr + write_size, udata);
+            r = (*pf)(iface->board, fw, addr + write_size, code_size, udata);
             if (r)
                 return r;
         }
@@ -622,10 +645,10 @@ static int teensy_upload(ty_board_interface *iface, ty_firmware *fw,
 
 static int teensy_reset(ty_board_interface *iface)
 {
-    unsigned int halfkay_version = 0;
-    size_t block_size = 0;
+    unsigned int halfkay_version;
+    size_t code_size, block_size;
 
-    int r = get_halfkay_settings(iface->model, &halfkay_version, &block_size);
+    int r = get_halfkay_settings(iface->model, &halfkay_version, &code_size, &block_size);
     if (r < 0)
         return r;
 
@@ -646,9 +669,9 @@ static int teensy_reboot(ty_board_interface *iface)
             if (!r) {
                 /* Don't keep these settings, some systems (such as Linux) may reuse them and
                    the device will keep rebooting when opened. */
-                ty_error_mask(TY_ERROR_SYSTEM);
+                hs_error_mask(HS_ERROR_SYSTEM);
                 change_baudrate(iface->port, 115200);
-                ty_error_unmask();
+                hs_error_unmask();
             }
         } break;
 

src/libty/common.c
@@ -153,7 +153,6 @@ static const char *generic_error(int err)
         case TY_ERROR_RANGE: { return "Out of range error"; } break;
         case TY_ERROR_SYSTEM: { return "System error"; } break;
         case TY_ERROR_PARSE: { return "Parse error"; } break;
-        case TY_ERROR_FIRMWARE: { return "Firmware error"; } break;
 
         case TY_ERROR_OTHER: {} break;
     }

commit 64125882bb100190b3c0987e7b6b99ae4d94a8bd

Fix broken firmware compatibility check
This serious regression was introduced in 3d99668.
Fixes #19.

src/libty/board.c
@@ -565,7 +565,7 @@ static int select_compatible_firmware(ty_board *board, ty_firmware **fws, unsign
     }
 
     if (fws_count > 1) {
-        return ty_error(TY_ERROR_NOT_FOUND, "No firmware is compatible with '%s' (%s)",
+        return ty_error(TY_ERROR_UNSUPPORTED, "No firmware is compatible with '%s' (%s)",
                         board->tag, ty_models[board->model].name);
     } else if (fw_models_count) {
         char buf[256], *ptr;
@@ -615,18 +615,18 @@ static void unref_upload_firmware(void *ptr)
 static int run_upload(ty_task *task)
 {
     ty_board *board = task->u.upload.board;
-    ty_firmware *fw = NULL;
+    ty_firmware *fw;
     int flags = task->u.upload.flags, r;
 
     if (flags & TY_UPLOAD_NOCHECK) {
         fw = task->u.upload.fws[0];
-    } else {
-        ty_error_mask(TY_ERROR_NOT_FOUND);
+    } else if (ty_models[board->model].mcu) {
         r = select_compatible_firmware(board, task->u.upload.fws, task->u.upload.fws_count, &fw);
-        ty_error_unmask();
-        // Maybe we can identify the board and test the firmwares in bootloader mode?
-        if (r < 0 && r != TY_ERROR_NOT_FOUND)
+        if (r < 0)
             return r;
+    } else {
+        // Maybe we can identify the board and test the firmwares in bootloader mode?
+        fw = NULL;
     }
 
     ty_log(TY_LOG_INFO, "Uploading to board '%s' (%s)", board->tag, ty_models[board->model].name);