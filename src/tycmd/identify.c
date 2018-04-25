5b09065c1988c738f9225ac40b591fe31ca3d76f
Niels Martignène <niels.martignene@protonmail.com>
2018-04-25 20:37:29 +0200

    Support stdin '-' path in `tycmd identify`

--- src/tycmd/identify.c
+++ src/tycmd/identify.c
@@ -64,7 +64,8 @@ int identify(int argc, char *argv[])
         unsigned int fw_models_count = 0;
         int r;
 
-        r = ty_firmware_load_file(opt, NULL, identify_firmware_format, &fw);
+        r = ty_firmware_load_file(opt, !strcmp(opt, "-") ? stdin : NULL,
+                                  identify_firmware_format, &fw);
         if (!r)
             fw_models_count = ty_firmware_identify(fw, fw_models, TY_COUNTOF(fw_models));
         ty_firmware_unref(fw);
