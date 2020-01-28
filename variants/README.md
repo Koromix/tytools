dd9f43d2d414e2be6cf8b4a5c7c07c57ada8b953
Niels Martignène <niels.martignene@protonmail.com>
2020-01-28 15:30:59 +0100

    Retrieve build variants in master branch

---
+++ variants/README.md
@@ -0,0 +1,2 @@
+Configure the project with CMake as usual but with `-DCONFIG_VARIANT=<name>` (e.g. morningstar) as an
+extra switch.
---
+++ variants/morningstar/variant.cmake
@@ -0,0 +1,10 @@
+set(CONFIG_PACKAGE_NAME "Morningstar Engineering Updater" CACHE STRING "")
+set(CONFIG_PACKAGE_FILENAME "MorningstarEngineeringUpdater" CACHE STRING "")
+
+set(CONFIG_TYCMD_BUILD OFF CACHE BOOL "")
+set(CONFIG_TYCOMMANDER_BUILD OFF CACHE BOOL "")
+set(CONFIG_TYUPDATER_EXECUTABLE "Morningstar Engineering Updater" CACHE STRING "")
+set(CONFIG_TYUPDATER_NAME "Morningstar Engineering Updater" CACHE STRING "")
+
+set(CONFIG_URL_WEBSITE "http://www.morningstarfx.com/" CACHE STRING "")
+set(CONFIG_URL_BUGS "" CACHE STRING "")
